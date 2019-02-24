1da77bebc571b351b0930f75801c4fb2d95edd24
AlfredPC <xjxtju@163.com>
2019-02-24 10:46:57 +0800

    merge

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,25 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <tabbar></tabbar>
   </div>
 </template>
-<style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
 
-#nav
-  padding 30px
-  a
-    font-weight bold
+<script type="text/ecmascript-6">
+  import tabbar from './components/tabbar'
+  export default {
+    name: "app",
+    components:{
+      tabbar
+    }
+  }
+</script>
+
+<style lang="stylus">
+  #app
+    font-family 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    text-align center
     color #2c3e50
-    &.router-link-exact-active
-      color #42b983
+
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import Vuex from 'vuex'
+import store from './store'
+import { MdToolbar } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
 
-Vue.use(Vuex)
-
+Vue.use(MdToolbar)
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
