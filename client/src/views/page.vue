18694b8638c13fbffaab343f476df2d9a7f3a29f
AlfredPC <xjxtju@163.com>
2019-02-28 16:52:10 +0800

    page&card

---
+++ client/src/views/page.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>this is page </p>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: "page"
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
